f67e0682724879078f8ffa9d8843ce63f5b58e53
vaibhavsharma308 <vs128646@gmail.com>
2025-01-15 13:15:32 +0530

    MVC Architecture Week 2.

---
+++ MVC Architecture_WEEK2/springbootwebtutorial/src/test/java/com/codingshuttle/springbootwebtutorial/springbootwebtutorial/SpringbootwebtutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingshuttle.springbootwebtutorial.springbootwebtutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootwebtutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
